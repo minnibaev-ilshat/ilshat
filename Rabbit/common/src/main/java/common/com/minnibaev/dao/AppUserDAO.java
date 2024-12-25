7ccb7f1fec25693685de2677c1362c380ae8dc25
ilshat <ilshat@192.168.100.225>
2024-12-25 18:56:06 +0700

    doesnt work common
    Change-Id: I0000000000000000000000000000000000000003

--- Rabbit/common/src/main/java/common/com/minnibaev/dao/AppUserDAO.java
+++ Rabbit/common/src/main/java/common/com/minnibaev/dao/AppUserDAO.java
@@ -3,10 +3,9 @@ package common.com.minnibaev.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import common.com.minnibaev.entity.AppUser;
 
-import java.util.Optional;
 
 public interface AppUserDAO extends JpaRepository<AppUser, Long> {
 
-    Optional<AppUser> findByTelegramUserId(Long id);
+	AppUser findByTelegramUserId(Long id);
 
 }
--- Rabbit/node/src/main/java/node/com/minnibaev/service/impl/MainServiceImpl.java
+++ Rabbit/node/src/main/java/node/com/minnibaev/service/impl/MainServiceImpl.java
@@ -3,7 +3,11 @@ package node.com.minnibaev.service.impl;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 
+import common.com.minnibaev.dao.AppUserDAO;
+import common.com.minnibaev.entity.AppUser;
+import common.com.minnibaev.entity.enums.UserState;
 import node.com.minnibaev.dao.RawDataDAO;
 import node.com.minnibaev.entity.RawData;
 import node.com.minnibaev.service.MainService;
@@ -14,22 +18,38 @@ public class MainServiceImpl implements MainService {
 
 	private final RawDataDAO rawDataDAO;
 	private final ProducerService producerService;
+	private final AppUserDAO appUserDAO;
 
-	public MainServiceImpl(RawDataDAO rawDataDAO, ProducerService producerService) {
+	public MainServiceImpl(RawDataDAO rawDataDAO, ProducerService producerService, AppUserDAO appUserDAO) {
 		super();
 		this.rawDataDAO = rawDataDAO;
 		this.producerService = producerService;
+		this.appUserDAO = appUserDAO;
 	}
 
 	@Override
 	public void processTextMessage(Update update) {
 		saveRawData(update);
+		var telegramUser = update.getMessage().getFrom();
+		var appUser = findOrSaveAppUser(telegramUser);
 
 		SendMessage sendMessage = new SendMessage();
 		sendMessage.setChatId(update.getMessage().getChatId());
 		sendMessage.setText("message from NODE");
+		System.out.println("NODE has sent message");
 		producerService.producerAnswer(sendMessage);
 
+	}
+
+	public AppUser findOrSaveAppUser(User telegramUser) {
+		AppUser persistentUser = appUserDAO.findByTelegramUserId(telegramUser.getId());
+		if (persistentUser == null) {
+			AppUser newUser = AppUser.builder().firstName(telegramUser.getFirstName())
+					.lastName(telegramUser.getLastName()).username(telegramUser.getUserName()).isActive(true)
+					.state(UserState.BASIC_STATE).build();
+			return appUserDAO.save(newUser);
+		}
+		return persistentUser;
 	}
 
 	private void saveRawData(Update update) {
