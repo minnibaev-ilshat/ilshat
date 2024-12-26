1e84e5133bfdf19500bfd91e711ca57e6447059b
ilshat <ilshat@192.168.100.225>
2024-12-26 12:47:05 +0700

    common works
    Change-Id: I0000000000000000000000000000000000000004

--- Rabbit/node/src/main/java/node/com/minnibaev/NodeApplication.java
+++ Rabbit/node/src/main/java/node/com/minnibaev/NodeApplication.java
@@ -2,11 +2,18 @@ package node.com.minnibaev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = { "node.*" , "common.*" })
+@ComponentScan(basePackages = { "node.*" , "common.*" })
+@EntityScan(basePackages = {"node.*" , "common.*"})
 @SpringBootApplication
 public class NodeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(NodeApplication.class);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(NodeApplication.class);
+	}
 }
--- Rabbit/node/src/main/java/node/com/minnibaev/service/impl/MainServiceImpl.java
+++ Rabbit/node/src/main/java/node/com/minnibaev/service/impl/MainServiceImpl.java
@@ -1,30 +1,33 @@
 package node.com.minnibaev.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 
-import common.com.minnibaev.dao.AppUserDAO;
 import common.com.minnibaev.entity.AppUser;
 import common.com.minnibaev.entity.enums.UserState;
+import common.com.minnibaev.dao.AppUserDAO;
 import node.com.minnibaev.dao.RawDataDAO;
 import node.com.minnibaev.entity.RawData;
 import node.com.minnibaev.service.MainService;
 import node.com.minnibaev.service.ProducerService;
 
+//@EnableJpaRepositories()
+//@EntityScan(basePackages = "Rabbit")
 @Service
 public class MainServiceImpl implements MainService {
 
 	private final RawDataDAO rawDataDAO;
 	private final ProducerService producerService;
-	private final AppUserDAO appUserDAO;
+	private final AppUserDAO appUserDAONode;
 
-	public MainServiceImpl(RawDataDAO rawDataDAO, ProducerService producerService, AppUserDAO appUserDAO) {
-		super();
+	@Autowired
+	public MainServiceImpl(RawDataDAO rawDataDAO, ProducerService producerService, AppUserDAO appUserDAONode) {
 		this.rawDataDAO = rawDataDAO;
 		this.producerService = producerService;
-		this.appUserDAO = appUserDAO;
+		this.appUserDAONode = appUserDAONode;
 	}
 
 	@Override
@@ -42,12 +45,12 @@ public class MainServiceImpl implements MainService {
 	}
 
 	public AppUser findOrSaveAppUser(User telegramUser) {
-		AppUser persistentUser = appUserDAO.findByTelegramUserId(telegramUser.getId());
+		AppUser persistentUser = appUserDAONode.findByTelegramUserId(telegramUser.getId());
 		if (persistentUser == null) {
 			AppUser newUser = AppUser.builder().firstName(telegramUser.getFirstName())
-					.lastName(telegramUser.getLastName()).username(telegramUser.getUserName()).isActive(true)
-					.state(UserState.BASIC_STATE).build();
-			return appUserDAO.save(newUser);
+					.lastName(telegramUser.getLastName()).username(telegramUser.getUserName())
+					.telegramUserId(telegramUser.getId()).isActive(true).state(UserState.BASIC_STATE).build();
+			return appUserDAONode.save(newUser);
 		}
 		return persistentUser;
 	}
